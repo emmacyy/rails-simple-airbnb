ad48f2a9a5ff55f36ed75cc8b8aeec2743a3eb8d
Emma Cheng <emmayauyau@gmail.com>
2022-02-16 16:54:44 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  puts "starting flat creation"
+  flat = Flat.create!(
+    name: Faker::Superhero.name,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.yoda,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..10)
+  )
+  puts "finished creating flat #{flat.id}"
+end
